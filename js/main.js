c506469a43df645653be0a6e536428536d5ea46b
avaz-rustamov <avaz.dev@icloud.com>
2022-02-05 09:20:12 +0500

    Add form

---
+++ js/main.js
@@ -0,0 +1,46 @@
+"use strick";
+// console.log(Boolean(0));
+// console.log(Boolean(""));
+// console.log(Boolean(undefined));
+// console.log(Boolean(null));
+// console.log(Boolean(NaN));
+// console.log(Boolean(false));
+// let name = prompt("Ismingiz kiriting...");
+// let naimaxon = "";
+
+// if (naimaxon) {
+//   alert(`${name} siz 18 yoshdan kattasiz`);
+// }else {
+//   alert(`${name} 18 yoshdan kichiksiz`);
+// }
+
+// let hasDriverLicence = true;
+// let hasDriverVision = true;
+// let driverName = prompt("Isnimgizni kiriting...", 'Naimaxon');
+// driverName = driverName.trim()
+
+// if (!(hasDriverLicence || hasDriverVision)) {
+//     console.log(`${driverName} siz mashina haydashingiz mumkun`);
+// }else {
+//     console.log(`${driverName} mashina haydashingiz mumkun emas`);
+// }
+
+// let enlishIELTS = false;
+// let mathExam = true;
+// let networking = true;
+
+// if (enlishIELTS && mathExam) {
+//       console.log("Tabriklayman Siz o'qishga kirdingiz.");
+//       console.log("Knowlege")
+// }else if ((enlishIELTS && mathExam) || networking) {
+//       console.log("Tabriklayman Siz o'qishga kirdingiz.");
+//       console.log("Tanka")
+// }else if (enlishIELTS && mathExam){
+//       console.log("Siz o'qishga kira olmadingiz.")
+// }else {
+//       console.log("Toto'g'ri qiymat kiritingiz.")
+// }
+
+let elHeading2 = document.querySelector(".heading2");
+
+console.log(heading2);
